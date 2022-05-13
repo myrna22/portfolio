87b49e4804ec935b05b915a8192dbf0cae06723a
myrna22 <mirnazayada80@yahoo.com>
2022-05-13 19:07:09 +0200

    Updates

---
+++ precache-manifest.f25257c6ef005b91472cbd3128c510e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a3245a678b8283603c7d1e9cab1d6a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3cb447ff5cb7d8663aec",
+    "url": "/portfolio/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "f9ee2411f24cb2f83b30",
+    "url": "/portfolio/static/css/main.b7a89953.chunk.css"
+  },
+  {
+    "revision": "3cb447ff5cb7d8663aec",
+    "url": "/portfolio/static/js/2.b9fec55b.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/portfolio/static/js/2.b9fec55b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9ee2411f24cb2f83b30",
+    "url": "/portfolio/static/js/main.5eb96f69.chunk.js"
+  },
+  {
+    "revision": "7ed53fe796af37015373",
+    "url": "/portfolio/static/js/runtime-main.bd3ef9be.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/portfolio/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/portfolio/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "5d425010104d1f9c0c7e5ec8193bffb7",
+    "url": "/portfolio/static/media/me.5d425010.jpg"
+  }
+]);
